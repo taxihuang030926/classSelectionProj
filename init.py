5831d14c816ecf75928a13fa51be0291a722db4e
Orkin <yang920916@gmail.com>
2024-04-12 02:01:21 +0800

    connect_db

--- init.py
+++ init.py
@@ -11,11 +11,59 @@ class Student:
     
 
 class Courses:
-    def __init__(self, Course_ID, Course_Name, Course_Description, Course_Credit):
+    def __init__(self, Course_ID, Course_Name, Course_Dept, Course_Description, Course_Credit):
         self.ID = Course_ID
         self.Name = Course_Name
-        self.Desc = Course_Description
+        self.Dept = Course_Dept
+        self.Description = Course_Description
         self.Credit = Course_Credit
         
     def get_class_data(self):
-        return " Class_ID: {}, Class_Name :{}, Credit: {}, Desc: {}".format(self.ID, self.Name, self.Credit, self.Desc)
+        return " Class_ID: {}, Class_Name :{}, Dept: {}, Desc: {}, Credit".format(self.ID, self.Name, self.dept, self.Description, self.Credit)
+ 
+class Time_slot:
+    def __init__(self, TS_ID, Session, Course_ID):
+        self.TS_ID = TS_ID
+        self.Session = Session
+        self.CourseID = Course_ID
+        
+    def get_time_slot(self):
+        return "TS_ID: {}, Session: {}, Course_ID: {}".format(self.TS_ID, self.Session, self.CourseID)
+    
+class Section:
+    def __init__(self, SEC_ID, Semester, Year):
+        self.SecID = SEC_ID
+        self.Semester = Semester
+        self.Year = Year
+        
+    def get_Section(self):
+        return "SEC_ID: {}, Semester: {}, Year: {}".format(self.SecID, self.Semester, self.Year)
+    
+class Instructor:
+    def __init__(self, I_ID, Name, dept):
+        self.Inst_ID = I_ID
+        self.Name = Name
+        self.dept = dept
+        
+    def get_Instructor(self):
+        return "Instructor_ID: {}, Name: {}, Instructor_dept: {}".format(self.Inst_ID, self.Name, self.dept)
+    
+class Department:
+    def __init__(self, Dept_Name, Building):
+        self.DeptName = Dept_Name
+        self.Building = Building
+        
+    def get_dept(self):
+        return "Dept_Name: {}, Building: {}".format(self.DeptName, self.Building)
+    
+class Classroom:
+    def __init__(self, Room_number, Capacity, Building):
+        self.RoomNumber = Room_number
+        self.Capacity = Capacity
+        self.Building = Building
+        
+    def get_Classroom(self):
+        return "Room_Number: {}, Capacity: {}, Building: {}".format(self.RoomNumber, self.Capacity, self.Building)
+        
+
+    
