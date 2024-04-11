efb680c9cb13bff7836515d00ee4b290627677a5
Tim Huang <taxihuang030926@gmail.com>
2024-04-12 03:16:26 +0800

    fix: form post

--- main.py
+++ main.py
@@ -1,62 +1,68 @@
-#以下是我的程式碼:
 from flask import Flask, request,render_template,redirect,session
-import os
-import MySQLdb
+import yaml
+import mysql.connector as mc
+import search, dct_user, dct_pwd
 
-currentlocation = os.path.dirname(os.path.abspath(__file__))
+def load(filename="config.yml"):
+    with open(filename, "r", encoding="utf-8") as config_file:
+        return yaml.load(config_file, Loader=yaml.Loader)
+    
+import_data = load()
 
 app = Flask(__name__)
 
-def connectServer():
-    conn = MySQLdb.connect(host="https://snapshot-medal-fathers-nested.trycloudflare.com/",
-                           user="admint",
-                           passwd="12341234",
-                           db="classselectionproj")
-    return conn
+conn = mc.connect(host=import_data.get('database', {}).get('host', ''),
+                  port=import_data.get('database', {}).get('port', ''),
+                  user=import_data.get('database', {}).get('user', ''),
+                  passwd=import_data.get('database', {}).get('password', ''),
+                  database=import_data.get('database', {}).get('database', ''))
 
+# testcursor = conn.cursor()
+# testcursor.execute("SELECT * FROM Student")
+# result = testcursor.fetchall()
+# for i in result:
+#     print(i)
+
+# login
 @app.route("/")
 def homepage():
     return render_template("index.html")
 
 @app.route("/",methods = ["POST"])
 def checklogin():
+    cursor = conn.cursor()
+
     UN = request.form.get('Username')
     PW = request.form.get('Password')
-
-    sqlconnection = connectServer()
-    curser = sqlconnection.cursor()
-    query1 = "SELECT S_ID, S_pwd FROM Student WHERE S_ID = {un} AND S_pwd= {pw})".format(un=UN, pw=PW)
-
-    rows = curser.execute(query1)
-    rows = rows.fetchall()
-    if len(rows) == 1:
-        return redirect
-    else:
-        return redirect("/")
     
+    # query1 = 'SELECT * FROM Student'
+    query1 = 'SELECT S_ID, S_pwd FROM Student WHERE S_ID="{un}" AND S_pwd="{pw}";'.format(un=UN, pw=PW)
+    
+    cursor.execute(query1)
+    result = cursor.fetchall()
+
+    if len(result) == 1:
+
+        return redirect("/search")
+    else:
+        return redirect ("/")
+    
+# search
 @app.route("/search")
 def searchpage():
     return render_template("search.html")
 
-# @myapp.route("/register",methods = ["GET","POST"])
-# def registerpage():
-#     if request.method == "POST":
-#         dUN = request.form['DUsername']
-#         dPW = request.form['DPassword']
-#         Uemail = request.form['EmalUser']
-#         sqlconnection = sqlite3.connect(currentlocation + "\Login.db")
-#         curser = sqlconnection.cursor()
-#         query1 = "INSERT INTO Users VALUES('{u}','{p}','{e}')".format(u = dUN,d = dPW,e = Uemail)
-#         curser.execute(query1)
-#         sqlconnection.commit()
-#         return redirect("/Login")
-#     return render_template("Register.html")
+    # print out course table
+
+    # follow
+
+    # 
+
+# enroll and slot 
+@app.route("/enrollment")
+def enrollmentpage():
+    return render_template("enrollment.html")
+
 
 if __name__ == "__main__":
-    app.run()
-
-
-
-# 請幫我根據圖片以及程式碼找出為什麼執行後網頁會顯示Internal Server Error
-# The server encountered an internal error and was unable to complete your request. Either the server is overloaded or there is an error in the application.
-# 的錯誤
+    app.run(debug=True)
