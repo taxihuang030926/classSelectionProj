1523dddca4a144c0e401e576c76bc481defb685e
dodoooooooo <126126864+dodoooooooo@users.noreply.github.com>
2024-04-16 14:24:42 +0800

    Update course.sql

--- db/course.sql
+++ db/course.sql
@@ -7,9 +7,9 @@ CREATE TABLE Courses(
     Category VARCHAR(20)
 );
 
-insert into Courses(Course_ID, Course_Name, dept, Course_Description, Course_Credit) values(1312, "系統程式",  "資訊", "宗杰", 3, "必修");
-insert into Courses(Course_ID, Course_Name, dept, Course_Description, Course_Credit) values(1314, "機率與統計",  "資訊", "景盛", 3, "必修");
-insert into Courses(Course_ID, Course_Name, dept, Course_Description, Course_Credit) values(1313, "資料庫系統",  "資訊", "懷中", 3, "必修");
-insert into Courses(Course_ID, Course_Name, dept, Course_Description, Course_Credit) values(1323, "互聯網路",  "資訊", "宗杰", 3, "選修");
+insert into Courses(Course_ID, Course_Name, dept, Course_Description, Course_Credit, Category) values(1312, "系統程式",  "資訊", "宗杰", 3, "必修");
+insert into Courses(Course_ID, Course_Name, dept, Course_Description, Course_Credit, Category) values(1314, "機率與統計",  "資訊", "景盛", 3, "必修");
+insert into Courses(Course_ID, Course_Name, dept, Course_Description, Course_Credit, Category) values(1313, "資料庫系統",  "資訊", "懷中", 3, "必修");
+insert into Courses(Course_ID, Course_Name, dept, Course_Description, Course_Credit, Category) values(1323, "互聯網路",  "資訊", "宗杰", 3, "選修");
 
 select * from Courses;
