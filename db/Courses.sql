52c9175dccdab165e32ec2256b6c82ac1c22adb2
Tim Huang <taxihuang030926@gmail.com>
2024-04-18 01:14:14 +0800

    update Courses table

--- db/Courses.sql
+++ db/Courses.sql
@@ -5,7 +5,7 @@ CREATE TABLE Courses(
     Course_Description VARCHAR(100),
     Course_Credit INT
 );
-
+/* TODO: 節數 日期 */
 insert into Courses(Course_ID, Course_Name, dept, Course_Description, Course_Credit) values(1312, "系統程式",  "資訊", "宗杰", 3);
 insert into Courses(Course_ID, Course_Name, dept, Course_Description, Course_Credit) values(1314, "機率與統計",  "資訊", "景盛", 3);
 insert into Courses(Course_ID, Course_Name, dept, Course_Description, Course_Credit) values(1313, "資料庫系統",  "資訊", "懷中", 3);
