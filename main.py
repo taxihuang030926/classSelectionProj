27cda23cbf25819ffdec13e45f1370e05915fef1
Tim Huang <taxihuang030926@gmail.com>
2024-04-18 21:09:58 +0800

    config search

--- main.py
+++ main.py
@@ -58,43 +58,73 @@ def search():
     # w\ multi select boxes: concatenate the selected values into a string and pass it to the query
     # no select boxes: no need to use the join method
     cursor = conn.cursor()
-
+    SC = request.form.get('Select-Code')
+    SD = request.form.get('Select-Day')
+    SCN = request.form.get('Select-Coursename')
+    SI = request.form.get('Select-Instructorname')
     CODE = request.form.get('Code')
     DAY = request.values.get('Day')
     CNAME = request.form.get('Coursename')
     INAME = request.form.get('Instructorname')
-    print(CODE, DAY, CNAME, INAME)
-
-    query = 'SELECT * FROM courses WHERE '
+    print(SC, SD, SCN, SI, CODE, DAY, CNAME, INAME)
+    flag = 0
+    cresult = []
+    query = 'SELECT courses.*, Course_Session.Session_Day, Course_Session.Session_RTime, course_session.Classroom,course_session.Session_ID FROM Courses, Course_Session WHERE '
     print("before query")
 
     # print out course table
-    if(CODE):
-        print("cond1")
-        query += 'course_id="{code}"'.format(code=CODE)
+    if CODE and SC == "on":
+        if not flag:
+            print("cond1")
+            flag = 1
+            query += 'courses.course_id="{code}" AND course_session.course_id="{code}"'.format(code=CODE)
+        else:
+            print("cond1")
+            query += ' AND courses.course_id="{code}" AND course_session.course_id="{code}"'.format(code=CODE)
 
-    if(DAY):
-        print("cond2")
-        query += 'day="{day}"'.format(day=DAY)
-    
+    if DAY and SD == "on":
+        if not flag:
+            print("cond2")
+            flag = 1
+            query += 'course_session.session_day="{day}"'.format(day=DAY)
+        else:
+            print("cond2")
+            query += ' AND course_session.session_day="{day}"'.format(day=DAY)
+
+    if CNAME and SCN == "on":
+        if not flag:
+            print("cond3")
+            flag = 1
+            query += 'courses.course_name LIKE "%{cname}%"'.format(cname=CNAME)
+        else:
+            print("cond3")
+            query += ' AND courses.course_name LIKE "%{cname}%"'.format(cname=CNAME)
+
+    if INAME and SI == "on":
+        if not flag:
+            print("cond4")
+            flag = 1
+            query += 'courses.instructor LIKE "%{iname}%"'.format(iname=INAME)
+        else:
+            print("cond4")
+            query += ' AND courses.instructor LIKE "%{iname}%"'.format(iname=INAME)
+
     if not (CODE or DAY or CNAME or INAME):
         query += '""'
+    else:
+        cursor.execute(query + ' AND Course_Session.Course_ID=Courses.Course_ID ORDER BY Course_Session.Session_ID;')
+        cresult = cursor.fetchall()
+        query += ' AND Course_Session.Course_ID=Courses.Course_ID AND Course_Session.Session_ID LIKE "%-1%" ORDER BY Course_Session.Session_ID '
     
     query += ';'
     print(query)
     cursor.execute(query)
     result = cursor.fetchall()
     print(result)
+    print("cresult: ")
+    print(cresult)
     
-    return render_template("search.html", courses=result)
-
-# follow
-
-
-    # 
-
-    cursor.close()
-    return redirect("/search")
+    return render_template("search.html", courses=result, cresults=cresult)
 
 # enroll and slot 
 @app.route("/enroll")
