8e4f8a2caab2e9eb2ead89fdd44c5bd6370f09fa
yang920916 <125525092+yang920916@users.noreply.github.com>
2024-04-21 23:45:12 +0800

    Update student.sql

--- db/student.sql
+++ db/student.sql
@@ -9,8 +9,11 @@ create table Student(
 );
 
 insert into Student(S_ID, Name, Ttl_Credit, S_pwd, Dept, Grade, Class) values("D1149711", "do-ning", 0, "dooooning666", "資訊", 2, 2);
-insert into Student(S_ID, Name, Ttl_Credit, S_pwd, Dept, Grade, Class) values("D1100001", "Std_A", 0, "Std_A", "資訊", 2, 3);
-insert into Student(S_ID, Name, Ttl_Credit, S_pwd, Dept, Grade, Class) values("D1100002", "Std_B", 0, "Std_B", "資訊", 1, 2);
-insert into Student(S_ID, Name, Ttl_Credit, S_pwd, Dept, Grade, Class) values("D1100003", "Std_C", 0, "Std_C", "資訊", 1, 3);
-
+insert into Student(S_ID, Name, Ttl_Credit, S_pwd, Dept, Grade, Class) values("D1100001", "Std_A", 0, "123", "資訊", 2, 3);
+insert into Student(S_ID, Name, Ttl_Credit, S_pwd, Dept, Grade, Class) values("D1100002", "Std_B", 0, "123", "資訊", 1, 2);
+insert into Student(S_ID, Name, Ttl_Credit, S_pwd, Dept, Grade, Class) values("D1100003", "Std_C", 0, "123", "資訊", 1, 3);
+insert into Student(S_ID, Name, Ttl_Credit, S_pwd, dept, Grade, Class) values("D1100004", "Std_D", 0, "123", "資訊", 2, 1);
+insert into Student(S_ID, Name, Ttl_Credit, S_pwd, dept, Grade, Class) values("D1100005", "Std_E", 0, "123", "資訊", 2, 2);
+insert into Student(S_ID, Name, Ttl_Credit, S_pwd, dept, Grade, Class) values("D1100006", "Std_F", 0, "123", "資訊", 2, 3);
+insert into Student(S_ID, Name, Ttl_Credit, S_pwd, dept, Grade, Class) values("D1100007", "Std_G", 0, "123", "資訊", 2, 4);
 select * from Student;
