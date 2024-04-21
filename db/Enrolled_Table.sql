61ca99e495da0a8aa701d4da318b019187fda24d
yang920916 <125525092+yang920916@users.noreply.github.com>
2024-04-21 23:44:59 +0800

    Update Enrolled_Table.sql

--- db/Enrolled_Table.sql
+++ db/Enrolled_Table.sql
@@ -81,8 +81,99 @@ insert into Enrolled_Table(S_ID, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10
                                 , S41, S42, S43, S44, S45, S46, S47, S48, S49, S50
                                 , S51, S52, S53, S54, S55, S56, S57, S58, S59, S60
                                 , S61, S62, S63, S64, S65, S66, S67, S68, S69, S70)
-                    values("D1149711", NULL, NULL, 1312, 1312, NULL, 1314, 1314, 1313, 1313, NULL, 1329, 1329, 1329, NULL, 
+                    values("D1149711", NULL, NULL, 1312, 1312, NULL, 1314, 1314, 1313, 1313, NULL, 1329, NULL, NULL, NULL, 
                                      NULL, NULL, 1313, 1314, NULL, NULL, NULL, NULL, 1311, NULL, NULL, NULL, NULL, NULL, 
                                      1324, 1324, 1324, 1312, NULL, 1323, 1323, 1323, 0811, 0811, 3528, 3528, NULL, NULL,
                                      3044, 3044, 2861, 2861, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
                                      3320, 3320, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into Enrolled_Table(S_ID, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10
+                                , S11, S12, S13, S14, S15, S16, S17, S18, S19, S20
+                                , S21, S22, S23, S24, S25, S26, S27, S28, S29, S30
+                                , S31, S32, S33, S34, S35, S36, S37, S38, S39, S40
+                                , S41, S42, S43, S44, S45, S46, S47, S48, S49, S50
+                                , S51, S52, S53, S54, S55, S56, S57, S58, S59, S60
+                                , S61, S62, S63, S64, S65, S66, S67, S68, S69, S70)
+                    values("D1100001", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+                                     
+insert into Enrolled_Table(S_ID, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10
+                                , S11, S12, S13, S14, S15, S16, S17, S18, S19, S20
+                                , S21, S22, S23, S24, S25, S26, S27, S28, S29, S30
+                                , S31, S32, S33, S34, S35, S36, S37, S38, S39, S40
+                                , S41, S42, S43, S44, S45, S46, S47, S48, S49, S50
+                                , S51, S52, S53, S54, S55, S56, S57, S58, S59, S60
+                                , S61, S62, S63, S64, S65, S66, S67, S68, S69, S70)
+                    values("D1100002", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+                                     
+insert into Enrolled_Table(S_ID, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10
+                                , S11, S12, S13, S14, S15, S16, S17, S18, S19, S20
+                                , S21, S22, S23, S24, S25, S26, S27, S28, S29, S30
+                                , S31, S32, S33, S34, S35, S36, S37, S38, S39, S40
+                                , S41, S42, S43, S44, S45, S46, S47, S48, S49, S50
+                                , S51, S52, S53, S54, S55, S56, S57, S58, S59, S60
+                                , S61, S62, S63, S64, S65, S66, S67, S68, S69, S70)
+                    values("D1100003", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into Enrolled_Table(S_ID, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10
+                                , S11, S12, S13, S14, S15, S16, S17, S18, S19, S20
+                                , S21, S22, S23, S24, S25, S26, S27, S28, S29, S30
+                                , S31, S32, S33, S34, S35, S36, S37, S38, S39, S40
+                                , S41, S42, S43, S44, S45, S46, S47, S48, S49, S50
+                                , S51, S52, S53, S54, S55, S56, S57, S58, S59, S60
+                                , S61, S62, S63, S64, S65, S66, S67, S68, S69, S70)
+                    values("D1100004", NULL, NULL, 1310, 1310, 1309, 1309, 1308, 1308, NULL, NULL, NULL, NULL, NULL, NULL, 
+                                     NULL, NULL, 1309, 1310, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+                                     NULL, NULL, NULL, 1308, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into Enrolled_Table(S_ID, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10
+                                , S11, S12, S13, S14, S15, S16, S17, S18, S19, S20
+                                , S21, S22, S23, S24, S25, S26, S27, S28, S29, S30
+                                , S31, S32, S33, S34, S35, S36, S37, S38, S39, S40
+                                , S41, S42, S43, S44, S45, S46, S47, S48, S49, S50
+                                , S51, S52, S53, S54, S55, S56, S57, S58, S59, S60
+                                , S61, S62, S63, S64, S65, S66, S67, S68, S69, S70)
+                    values("D1100005", NULL, NULL, 1312, 1312, NULL, 1314, 1314, 1313, 1313, NULL, NULL, NULL, NULL, NULL, 
+                                     NULL, NULL, 1314, 1313, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+                                     NULL, NULL, NULL, 1312, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into Enrolled_Table(S_ID, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10
+                                , S11, S12, S13, S14, S15, S16, S17, S18, S19, S20
+                                , S21, S22, S23, S24, S25, S26, S27, S28, S29, S30
+                                , S31, S32, S33, S34, S35, S36, S37, S38, S39, S40
+                                , S41, S42, S43, S44, S45, S46, S47, S48, S49, S50
+                                , S51, S52, S53, S54, S55, S56, S57, S58, S59, S60
+                                , S61, S62, S63, S64, S65, S66, S67, S68, S69, S70)
+                    values("D1100006", NULL, NULL, 1317, 1317, NULL, 1316, 1316, 1318, 1318, NULL, NULL, NULL, NULL, NULL, 
+                                     NULL, NULL, 1318, 1317, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+                                     NULL, NULL, 1316, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into Enrolled_Table(S_ID, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10
+                                , S11, S12, S13, S14, S15, S16, S17, S18, S19, S20
+                                , S21, S22, S23, S24, S25, S26, S27, S28, S29, S30
+                                , S31, S32, S33, S34, S35, S36, S37, S38, S39, S40
+                                , S41, S42, S43, S44, S45, S46, S47, S48, S49, S50
+                                , S51, S52, S53, S54, S55, S56, S57, S58, S59, S60
+                                , S61, S62, S63, S64, S65, S66, S67, S68, S69, S70)
+                    values("D1100007", NULL, NULL, 1320, 1320, NULL, 1322, 1322, 1321, 1321, NULL, NULL, NULL, NULL, NULL, 
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+                                     NULL, NULL, 1320, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                     NULL, 1322, NULL, NULL, NULL, 1321, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
